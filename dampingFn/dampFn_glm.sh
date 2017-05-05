dd915ac1c59dba57754e66f9ee69f1520ef98d23
RichGraham <Richard.Graham@nottingham.ac.uk>
2017-05-05 19:15:26 +0100

    Added example list file

--- dampingFn/dampFn_glm.sh
+++ dampingFn/dampFn_glm.sh
@@ -5,21 +5,24 @@
 if (( $# < 2 )); then
     echo "Constructs a damping function from GLaMM model output files" >&2
     echo " ">&2
-   echo "Usage  : dampFn_glm.sh [line number in output file to use][*.list file of input files]" >&2
+   echo "Usage  : dampFn_glm.sh [*.list file of input files] [line number in output file to use]" >&2
    echo "Example:  dampFn_glm.sh 1000 Cv0-0.list" >&2
   exit 1
 fi
 
 #====Extract arguments====
-LineNumber=$1
-ListFile=$2
+ListFile=$1
+LineNumber=$2
+
 
 arr1=( $( cut -d' ' -f1 $ListFile ) )
+arr2=( $( cut -d' ' -f2 $ListFile ) )
 
 for i in $(seq 1 $(expr ${#arr1[@]} - 1))
 do
     FileName=${arr1[$i]}
-    echo Input file: $FileName
+    Strain=${arr2[$i]}
+    echo Input file: $FileName $Strain
     sed -n 3,3p 2nd40$FileName.dat 
 
 done
